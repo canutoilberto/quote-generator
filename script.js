74cc963c2d63477e14c563212945196453141484
canutoilberto <canutoilberto@gmail.com>
2024-02-28 11:00:07 -0300

    Functions Added

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+  //Pick randon quote
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //Check if the author field is blank
+  if (!quote.author || quote.author === "type.fit") {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //Check the quote length
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+}
+
 // Get Quotes from API
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+    alert(error);
+  }
 }
+
+//Tweet a Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+//On Load
+getQuotes();
