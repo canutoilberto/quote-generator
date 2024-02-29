6822f71bb00b19c4b120bd551c225bf206637a50
canutoilberto <canutoilberto@gmail.com>
2024-02-29 09:43:26 -0300

    [Fix] Improve better names to variables

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
-// Show Loading
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
-  quoteContainer.hidden = false;
-  loader.hidden = true;
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
 }
 
-//Show New Quote
+//Get Quote from API
 function newQuote() {
-  loading();
-  //Pick randon quote
+  showLoadingSpinner();
+  //Pick a randon quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //Check if the author field is blank
   if (!quote.author || quote.author === "type.fit") {
@@ -39,12 +39,12 @@ function newQuote() {
   }
   //Set quote, hide loader
   quoteText.textContent = quote.text;
-  complete();
+  removeLoadingSpinner();
 }
 
 // Get Quotes from API
 async function getQuotes() {
-  loading();
+  showLoadingSpinner();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
